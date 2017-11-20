15f72d11697b4da6107ec4e805293f092868b249
Junis <32417894+junisss@users.noreply.github.com>
2017-11-20 11:42:28 +0100

    Minor improvements in JS code

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,12 @@
 function saveItemToList() {
     const getUserItemValue = document.getElementById('newItem').value;
-    if (getUserItemValue == 0) {
-        console.log('Nie podales wartosci')
-    } else {
+    if (!getUserItemValue) {
+        return;} 
+        
     const createListItem = document.createElement('li');
     const copyTextToList = document.createTextNode(getUserItemValue);
     createListItem.appendChild(copyTextToList);
     document.getElementById('listOfItems').appendChild(createListItem);
     };
 }
-    
-    /* Erase the text input when next click */
-$('input:text').focus(
-    function eraseInput(){
-        $(this).val('');
-    });
-
-saveItemToList();
+ 
